63583d3f59ede56c36e8051c4ac368fd8b505685
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-08 15:00:38 +0900

    .

--- main/java/com/models/dao/MemberDao.java
+++ main/java/com/models/dao/MemberDao.java
@@ -1,5 +1,22 @@
 package com.models.dao;
 
-public class MemberDao {
+import javax.servlet.http.HttpServletRequest;
+import java.sql.*;
 
+import com.core.*;
+/**
+ * 회원 Model
+ *
+ */
+public class MemberDao {
+	/**
+	 * 회원 가입 처리 
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public boolean join(HttpServletRequest request) {
+		
+		return true;
+	}
 }
