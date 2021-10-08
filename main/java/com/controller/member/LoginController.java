0f4b611f21c5a1de4204166976dd9fd5da8fad3b
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-08 10:17:07 +0900

    .

---
+++ main/java/com/controller/member/LoginController.java
@@ -0,0 +1,31 @@
+package com.controller.member;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * 로그인  
+ * GET - 로그인 양식, POST - 로그인 처리
+ *  
+ */
+public class LoginController extends HttpServlet {
+	
+	/** 로그인 양식 */
+	@Override 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	/** 로그인 처리 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
+
+
+
