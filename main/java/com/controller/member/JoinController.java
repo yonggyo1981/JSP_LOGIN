def5ea7db213c5ed0c7d0ee68a9f7c8e28bda21f
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-08 09:51:38 +0900

    .

---
+++ main/java/com/controller/member/JoinController.java
@@ -0,0 +1,32 @@
+package com.controller.member;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * 회원 가입 
+ * GET - 가입 양식, POST - 가입 처리 
+ */
+public class JoinController extends HttpServlet {
+	
+	/** 회원가입 양식 */
+	@Override 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	/** 회원가입 처리 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
+
+
+
+
+
