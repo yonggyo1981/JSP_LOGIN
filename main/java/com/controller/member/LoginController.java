4c3812ee3df11243edf4381f521833f605e9f6e6
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-12 10:45:06 +0900

    .

--- main/java/com/controller/member/LoginController.java
+++ main/java/com/controller/member/LoginController.java
@@ -6,6 +6,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.models.dao.MemberDao;
+import com.exception.*;
 
 /**
  * 로그인  
@@ -25,7 +29,15 @@ public class LoginController extends HttpServlet {
 	/** 로그인 처리 */
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		try {
+			MemberDao dao = new MemberDao();
+			dao.login(request);
+			
+		} catch (AlertException e) {
+			out.print("<script>alert('" + e.getMessage() + "');</script>");
+		}
 	}
 }
 
